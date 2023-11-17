ba355689343c581e1404a11d3938f24dc2951f7f
Lucas Vinícius <lucasvsn19@gmail.com>
2023-11-17 10:52:47 -0300

    game repository, service, controller

---
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -0,0 +1,47 @@
+package com.devsuperior.dslist.dto;
+
+import com.devsuperior.dslist.entities.Game;
+
+public class GameMinDTO {
+
+	private Long id;
+	private String title;
+	private Integer year;
+	private String imgUrl;
+	private String shortDescription;
+
+	public GameMinDTO() {
+
+	}
+
+	public GameMinDTO(Game entities) {
+		id = entities.getId();
+		title = entities.getTitle();
+		year = entities.getYear();
+		imgUrl = entities.getImgUrl();
+		shortDescription = entities.getShortDescription();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+	
+	
+
+}
